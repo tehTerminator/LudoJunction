769d041e35999d2df0387e6af114f371f8017581
Prateek Kher <prateek.kher@gmail.com>
2020-09-16 22:17:32 +0530

    Added User Type Constants

--- src/app/shared/collection.ts
+++ src/app/shared/collection.ts
@@ -1,3 +1,12 @@
 export const SECOND = 1000;
 export const MINUTE = 60000;
 export const HOUR = 3600000;
+export const ADMINISTRATOR = 'ADMINISTRATOR';
+export const PLAYER = 'PLAYER';
+
+export interface SqlResponse<T> {
+    errors: Array<string>;
+    log: Array<string>;
+    status: boolean;
+    data: Array<T>
+}
