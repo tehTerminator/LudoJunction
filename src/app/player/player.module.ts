c1421128a72b08e4967c6555844b639139ef9dc3
Prateek Kher <prateek.kher@gmail.com>
2020-09-17 21:34:59 +0530

    Created Result and Transactions Page

--- src/app/player/player.module.ts
+++ src/app/player/player.module.ts
@@ -4,10 +4,12 @@ import { CommonModule } from '@angular/common';
 import { PlayerRoutingModule } from './player-routing.module';
 import { PlayerComponent } from './player.component';
 import { DashboardComponent } from './dashboard/dashboard.component';
+import { TransactionsComponent } from './transactions/transactions.component';
+import { ResultComponent } from './result/result.component';
 
 
 @NgModule({
-  declarations: [PlayerComponent, DashboardComponent],
+  declarations: [PlayerComponent, DashboardComponent, TransactionsComponent, ResultComponent],
   imports: [
     CommonModule,
     PlayerRoutingModule
