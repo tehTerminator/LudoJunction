12819f9ddb5b5330be685254b1a421ff063e3b70
Prateek Kher <prateek.kher@gmail.com>
2020-09-17 09:58:41 +0530

    Cleaned Up user.model.ts

--- src/app/shared/user.model.ts
+++ src/app/shared/user.model.ts
@@ -1,25 +1,13 @@
 export class User{
-    private id: number;
-    private _type: string;
-    private _token: string;
-    public title: string;
-    public username: string;
-    public generatedOn: number;
 
     constructor(
-        title: string,
-        username: string,
-        token: string,
-        generatedOn: number,
-		type: string
-    ) {
-        this.id = 0;
-        this.title = title;
-        this.username = username;
-	this._type = type;
-        this._token = token;
-        this.generatedOn = generatedOn;
-    }
+		private _id: number,
+        public title: string,
+        public username: string,
+        private _token: string,
+        public generatedOn: number,
+		private _type: string
+    ) { }
 
     get token(): string {
         const minTime = (new Date()).getTime() - HOUR;
@@ -34,3 +22,7 @@ export class User{
 	    return this._type.toUpperCase();
 	}
 
+	get id(): number{
+		return this._id;
+	}
+}
