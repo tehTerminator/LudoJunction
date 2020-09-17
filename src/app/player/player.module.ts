86e865ce7137ea800c32299a20f639b5c837e78e
Prateek Kher <prateek.kher@gmail.com>
2020-09-17 21:32:40 +0530

    Created Players Dashboard

--- src/app/player/player.module.ts
+++ src/app/player/player.module.ts
@@ -3,10 +3,11 @@ import { CommonModule } from '@angular/common';
 
 import { PlayerRoutingModule } from './player-routing.module';
 import { PlayerComponent } from './player.component';
+import { DashboardComponent } from './dashboard/dashboard.component';
 
 
 @NgModule({
-  declarations: [PlayerComponent],
+  declarations: [PlayerComponent, DashboardComponent],
   imports: [
     CommonModule,
     PlayerRoutingModule
