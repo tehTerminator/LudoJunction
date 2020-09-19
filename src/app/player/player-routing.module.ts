859e4c7b3ba3b4627b5f4bcd0ee14028d32ee008
Prateek Kher <prateek.kher@gmail.com>
2020-09-19 15:57:56 +0530

    Updated Player Routes

--- src/app/player/player-routing.module.ts
+++ src/app/player/player-routing.module.ts
@@ -2,8 +2,17 @@ import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 
 import { PlayerComponent } from './player.component';
+import { DashboardComponent } from './dashboard/dashboard.component';
+import { ResultComponent } from './result/result.component';
+import { TransactionsComponent } from './transactions/transactions.component';
 
-const routes: Routes = [{ path: '', component: PlayerComponent }];
+const routes: Routes = [
+  { path: '', component: PlayerComponent },
+  { path: 'dashboard', component: DashboardComponent },
+  { path: 'result/:id', component: ResultComponent },
+  { path: 'transactions', component: TransactionsComponent },
+  { path: '**', redirectTo: '', pathMatch: 'full' }
+];
 
 @NgModule({
   imports: [RouterModule.forChild(routes)],
