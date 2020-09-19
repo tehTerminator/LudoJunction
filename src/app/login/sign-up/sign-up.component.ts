3c5125c059ac7ec9ec6a67e954cf2bca3395ef6f
Prateek Kher <prateek.kher@gmail.com>
2020-09-19 13:34:26 +0530

    Fixed Imports and Typos

--- src/app/login/sign-up/sign-up.component.ts
+++ src/app/login/sign-up/sign-up.component.ts
@@ -1,40 +1,46 @@
 import { Component, OnInit } from '@angular/core';
+import { FormBuilder, Validators } from '@angular/forms';
+import { AuthService } from '../../shared/auth.service';
+import { SqlResponse } from '../../shared/collection';
 
 @Component({
-  selector: 'app-sign-up',
-  templateUrl: './sign-up.component.html',
-  styleUrls: ['./sign-up.component.css']
+	selector: 'app-sign-up',
+	templateUrl: './sign-up.component.html',
+	styleUrls: ['./sign-up.component.css']
 })
 export class SignUpComponent implements OnInit {
 	signUpForm: FormGroup;
 
-  constructor(
+	constructor(
 		private fb: FormBuilder,
 		private authService: AuthService
 	) { }
 
-  ngOnInit(): void {
+	ngOnInit(): void {
 		this.signUpForm = this.fb.group({
 			title: ['', Validators.required],
-			username: ['', Validators.required],
 			password: ['', Validators.required],
 			email: ['', Validators.required]
 		});
-  }
+	}
 
 	onSubmit() {
 		if (this.signUpForm.invalid) {
 			return;
 		}
 
-		this.authService.signUp(title, username, password, email)
-		.subscribe((res: SqlResponse<UserData>) => {
-			if (res.status) {
-				// Goto Login Page
-				// this.router.navigate(['/login']);
-			} else {
-				// Show Some Error Message
-			}
-		})
+		const title = this.signUpForm.get('title').value;
+		const email = this.signUpForm.get('email').value;
+		const password = this.signUpForm.get('password').value;
+
+		this.authService.signUp(title, email, password)
+			.subscribe((res: SqlResponse) => {
+				if (res.status) {
+					// Goto Login Page
+					// this.router.navigate(['/login']);
+				} else {
+					// Show Some Error Message
+				}
+			});
 	}
 }
