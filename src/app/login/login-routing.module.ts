ebe683e3b05fd28120fcfa4c553bd064d2fac46e
Prateek Kher <prateek.kher@gmail.com>
2020-09-19 13:29:58 +0530

    Added ReactiveFormsModule

--- src/app/login/login-routing.module.ts
+++ src/app/login/login-routing.module.ts
@@ -6,17 +6,18 @@ import { SignInComponent } from './sign-in/sign-in.component';
 import { SignUpComponent } from './sign-up/sign-up.component';
 
 const routes: Routes = [
-		{ 
-				path: '', 
-				component: LoginComponent, 
-				children: [
-					{path: 'signIn', component: SignInComponent},
-					{path: 'signUp', component: SignUpComponent},
-					{path: '**', redirectTo: 'signIn', pathMatch='full'}
-]}];
+	{
+		path: '', component: LoginComponent, children:
+			[
+				{ path: 'signIn', component: SignInComponent },
+				{ path: 'signUp', component: SignUpComponent },
+				{ path: '**', redirectTo: 'signIn', pathMatch: 'full' },
+			],
+	},
+];
 
 @NgModule({
-  imports: [RouterModule.forChild(routes)],
-  exports: [RouterModule]
+	imports: [RouterModule.forChild(routes)],
+	exports: [RouterModule],
 })
 export class LoginRoutingModule { }
