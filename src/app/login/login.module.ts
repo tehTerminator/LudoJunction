6fece9a867f2bb694e37bda464bffe9bed6be3a7
Prateek Kher <prateek.kher@gmail.com>
2020-09-19 13:30:08 +0530

    Fixed Spaces

--- src/app/login/login.module.ts
+++ src/app/login/login.module.ts
@@ -5,13 +5,15 @@ import { LoginRoutingModule } from './login-routing.module';
 import { LoginComponent } from './login.component';
 import { SignInComponent } from './sign-in/sign-in.component';
 import { SignUpComponent } from './sign-up/sign-up.component';
+import { ReactiveFormsModule } from '@angular/forms';
 
 
 @NgModule({
   declarations: [LoginComponent, SignInComponent, SignUpComponent],
   imports: [
     CommonModule,
-    LoginRoutingModule
+    LoginRoutingModule,
+    ReactiveFormsModule
   ]
 })
 export class LoginModule { }
