eb1290e7f42294fc08429522abe30af68d19bf63
Prateek Kher <prateek.kher@gmail.com>
2020-09-27 16:05:19 +0530

    Updated URL Environments

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -2,24 +2,24 @@ export const environment = {
   production: true,
   url: {
     user: {
-      signIn: 'http://ludojunction/api/api/user.signIn.php',
-      signUp: 'http://ludojunction/api/api/user.signUp.php',
-      activate: 'http://ludojunction/api/api/user.activate.php',
-      select: 'http://ludojunction/api/api/user.select.php'
+      signIn: 'http://ludojunction/api/user.signIn.php',
+      signUp: 'http://ludojunction/api/user.signUp.php',
+      activate: 'http://ludojunction/api/user.activate.php',
+      select: 'http://ludojunction/api/user.select.php'
     },
     balance: {
-      view: 'http://ludojunction/api/api/balance.view.php',
-      payin: 'http://ludojunction/api/api/balance.payin.php',
-      payout: 'http://ludojunction/api/api/balance.payout.php',
-      previousRequest: 'http://ludojunction/api/api/balance.previous.php'
+      view: 'http://ludojunction/api/balance.view.php',
+      payin: 'http://ludojunction/api/balance.payin.php',
+      payout: 'http://ludojunction/api/balance.payout.php',
+      previousRequest: 'http://ludojunction/api/balance.previous.php'
     },
     challenge: {
-      get: 'http://ludojunction/api/api/challenge.get.php',
-      create: 'http://ludojunction/api/api/challenge.create.php',
-      update: 'http://ludojunction/api/api/challenge.update.php',
-      accept: 'http://ludojunction/api/api/challenge.accept.php',
-      result: 'http://ludojunction/api/api/challenge.result.php'
+      get: 'http://ludojunction/api/challenge.get.php',
+      create: 'http://ludojunction/api/challenge.create.php',
+      update: 'http://ludojunction/api/challenge.update.php',
+      accept: 'http://ludojunction/api/challenge.accept.php',
+      result: 'http://ludojunction/api/challenge.result.php'
     },
-    transaction: 'http://ludojunction/api/api/transactions.php'
+    transaction: 'http://ludojunction/api/transactions.php'
   }
 };
