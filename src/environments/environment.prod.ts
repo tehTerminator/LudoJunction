d49cc44769af24043fd47002fd183959151b1fee
Prateek Kher <prateek.kher@gmail.com>
2020-09-27 16:04:54 +0530

    updated Environment Production

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -2,18 +2,24 @@ export const environment = {
   production: true,
   url: {
     user: {
-      signIn: 'http://ludojunction.com/api/user.signIn.php',
-      signUp: 'http://ludojunction.com/api/user.signUp.php',
-      activate: 'http://ludojunction.com/api/user.activate.php',
-      select: 'http://ludojunction.com/api/user.select.php'
+      signIn: 'http://ludojunction/api/api/user.signIn.php',
+      signUp: 'http://ludojunction/api/api/user.signUp.php',
+      activate: 'http://ludojunction/api/api/user.activate.php',
+      select: 'http://ludojunction/api/api/user.select.php'
+    },
+    balance: {
+      view: 'http://ludojunction/api/api/balance.view.php',
+      payin: 'http://ludojunction/api/api/balance.payin.php',
+      payout: 'http://ludojunction/api/api/balance.payout.php',
+      previousRequest: 'http://ludojunction/api/api/balance.previous.php'
     },
     challenge: {
-      get: 'http://ludojunction.com/api/challenge.get.php',
-      create: 'http://ludojunction.com/api/challenge.create.php',
-      update: 'http://ludojunction.com/api/challenge.update.php',
-      accept: 'http://ludojunction.com/api/challenge.accept.php',
-      result: 'http://ludojunction.com/api/challenge.result.php'
+      get: 'http://ludojunction/api/api/challenge.get.php',
+      create: 'http://ludojunction/api/api/challenge.create.php',
+      update: 'http://ludojunction/api/api/challenge.update.php',
+      accept: 'http://ludojunction/api/api/challenge.accept.php',
+      result: 'http://ludojunction/api/api/challenge.result.php'
     },
-    transaction: 'http://ludojunction.com/api/transactions.php'
+    transaction: 'http://ludojunction/api/api/transactions.php'
   }
 };
