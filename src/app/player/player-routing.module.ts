1f0ffe6f12770cce9d8d6030acea7dcc36e58c04
Prateek Kher <prateek.kher@gmail.com>
2020-10-10 15:02:54 +0530

    Created Dashboard and Routing

--- src/app/player/player-routing.module.ts
+++ src/app/player/player-routing.module.ts
@@ -3,7 +3,12 @@ import { Routes, RouterModule } from '@angular/router';
 
 import { PlayerComponent } from './player.component';
 
-const routes: Routes = [{ path: '', component: PlayerComponent }];
+const routes: Routes = [{ path: '', component: PlayerComponent, children: [
+				{ path: 'dashboard', component: DashboardComponent },
+				{ path: 'result', component: ResultComponent },
+				{ path: 'transactions', component: TransactionsComponent },
+				{ path: '**', redirectTo: 'dashboard', pathMatch: 'full }
+]}];
 
 @NgModule({
   imports: [RouterModule.forChild(routes)],
